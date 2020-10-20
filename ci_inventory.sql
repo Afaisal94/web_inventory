cd64dccd1fce81f0711f0afc5aa6a4738bad2409
Afaisal94 <a.faisal1994@gmail.com>
2020-10-20 17:21:55 +0700

    initial commit

---
+++ ci_inventory.sql
@@ -0,0 +1,294 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Apr 15, 2019 at 11:05 PM
+-- Server version: 10.1.38-MariaDB-cll-lve
+-- PHP Version: 7.2.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `skritfum_bintang`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `absensi`
+--
+
+CREATE TABLE `absensi` (
+  `kd_absen` int(10) NOT NULL,
+  `tgl_absen` date NOT NULL,
+  `waktu_absen` varchar(25) NOT NULL,
+  `kd_user` int(10) NOT NULL,
+  `absen` varchar(50) NOT NULL,
+  `keterangan` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `absensi`
+--
+
+INSERT INTO `absensi` (`kd_absen`, `tgl_absen`, `waktu_absen`, `kd_user`, `absen`, `keterangan`) VALUES
+(1, '2019-01-29', '10:10', 2, 'Masuk', 'Terlambat'),
+(2, '2019-02-03', '16:30', 2, 'Masuk', 'Terlambat'),
+(3, '2019-02-04', '07:24', 2, 'Masuk', 'Tepat Waktu'),
+(4, '2019-02-05', '10:50', 4, 'Masuk', 'Terlambat'),
+(5, '2019-02-06', '10:01', 4, 'Masuk', 'Terlambat');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `kd_barang` int(10) NOT NULL,
+  `kd_kategori` int(10) NOT NULL,
+  `nama_barang` varchar(255) NOT NULL,
+  `satuan` varchar(25) NOT NULL,
+  `stok` int(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`kd_barang`, `kd_kategori`, `nama_barang`, `satuan`, `stok`) VALUES
+(1, 1, 'Gate Valve', 'Pcs', 490),
+(2, 2, 'Module', 'Pcs', 250),
+(3, 2, 'Lamp 400 W', 'Pcs', 700),
+(4, 2, 'Conduit Pipe', 'Btg', 300),
+(5, 3, 'Oil For Turbine', 'Can', 40),
+(6, 3, 'Seal', 'Pcs', 200),
+(7, 4, 'Thermocouple', 'Pcs', 300),
+(8, 1, 'O Ring', 'Pcs', 800),
+(9, 4, 'Analyzer', 'Pcs', 400),
+(10, 4, 'Swagelok', 'Pcs', 100),
+(11, 1, 'u-bolt with washer', 'pcs', 150),
+(12, 2, 'Relay', 'set', 100);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang_keluar`
+--
+
+CREATE TABLE `barang_keluar` (
+  `kd_bk` int(10) NOT NULL,
+  `tgl_bk` date NOT NULL,
+  `kd_barang` int(10) NOT NULL,
+  `jumlah_bk` int(25) NOT NULL,
+  `keterangan` text NOT NULL,
+  `kd_user` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `barang_keluar`
+--
+
+INSERT INTO `barang_keluar` (`kd_bk`, `tgl_bk`, `kd_barang`, `jumlah_bk`, `keterangan`, `kd_user`) VALUES
+(1, '2019-02-13', 1, 10, '-', 2),
+(2, '2019-02-05', 5, 40, 'pemakaian restart turbine', 4),
+(3, '2019-02-06', 3, 10, 'test', 4),
+(4, '2019-02-06', 3, 20, 'coba', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang_masuk`
+--
+
+CREATE TABLE `barang_masuk` (
+  `kd_bm` int(10) NOT NULL,
+  `tgl_bm` date NOT NULL,
+  `kd_barang` int(10) NOT NULL,
+  `kd_suplayer` int(10) NOT NULL,
+  `jumlah_bm` int(25) NOT NULL,
+  `keterangan` text NOT NULL,
+  `kd_user` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `barang_masuk`
+--
+
+INSERT INTO `barang_masuk` (`kd_bm`, `tgl_bm`, `kd_barang`, `kd_suplayer`, `jumlah_bm`, `keterangan`, `kd_user`) VALUES
+(1, '2019-01-03', 3, 1, 30, 'request dari pak sukemi', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori_barang`
+--
+
+CREATE TABLE `kategori_barang` (
+  `kd_kategori` int(10) NOT NULL,
+  `nama_kategori` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kategori_barang`
+--
+
+INSERT INTO `kategori_barang` (`kd_kategori`, `nama_kategori`) VALUES
+(1, 'Piping'),
+(2, 'Electrical'),
+(3, 'Mechanical'),
+(4, 'Instrument');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `suplayer`
+--
+
+CREATE TABLE `suplayer` (
+  `kd_suplayer` int(10) NOT NULL,
+  `nama_suplayer` varchar(255) NOT NULL,
+  `alamat` text NOT NULL,
+  `no_telp` varchar(25) NOT NULL,
+  `keterangan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `suplayer`
+--
+
+INSERT INTO `suplayer` (`kd_suplayer`, `nama_suplayer`, `alamat`, `no_telp`, `keterangan`) VALUES
+(1, 'PT Chakra Sarana Sentosa', 'Jalan muara baru tanjung priuk', '021-8091011', 'supplier lampu dan alat elektronic');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `kd_user` int(10) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `level` int(10) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `kd_pegawai` int(10) NOT NULL,
+  `posisi` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`kd_user`, `username`, `password`, `level`, `nama`, `kd_pegawai`, `posisi`) VALUES
+(1, 'bobby', '21232f297a57a5a743894a0e4a801fc3', 1, 'Bobby', 13136775, 'Admin'),
+(2, 'john', '21232f297a57a5a743894a0e4a801fc3', 1, 'John', 13136776, 'Admin'),
+(3, 'frank', '21232f297a57a5a743894a0e4a801fc3', 2, 'Frank', 13136777, 'HRD'),
+(4, 'bintang', '21232f297a57a5a743894a0e4a801fc3', 1, 'Bintang', 1313200, 'Admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `absensi`
+--
+ALTER TABLE `absensi`
+  ADD PRIMARY KEY (`kd_absen`);
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`kd_barang`);
+
+--
+-- Indexes for table `barang_keluar`
+--
+ALTER TABLE `barang_keluar`
+  ADD PRIMARY KEY (`kd_bk`);
+
+--
+-- Indexes for table `barang_masuk`
+--
+ALTER TABLE `barang_masuk`
+  ADD PRIMARY KEY (`kd_bm`);
+
+--
+-- Indexes for table `kategori_barang`
+--
+ALTER TABLE `kategori_barang`
+  ADD PRIMARY KEY (`kd_kategori`);
+
+--
+-- Indexes for table `suplayer`
+--
+ALTER TABLE `suplayer`
+  ADD PRIMARY KEY (`kd_suplayer`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`kd_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `absensi`
+--
+ALTER TABLE `absensi`
+  MODIFY `kd_absen` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `kd_barang` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `barang_keluar`
+--
+ALTER TABLE `barang_keluar`
+  MODIFY `kd_bk` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `barang_masuk`
+--
+ALTER TABLE `barang_masuk`
+  MODIFY `kd_bm` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `kategori_barang`
+--
+ALTER TABLE `kategori_barang`
+  MODIFY `kd_kategori` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `suplayer`
+--
+ALTER TABLE `suplayer`
+  MODIFY `kd_suplayer` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `kd_user` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
